03613a79741f7490db1daddb6cafffedeb7b4799
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-06-23 14:43:15 -0400

    1

--- src/components/cv_page.js
+++ src/components/cv_page.js
@@ -1,8 +1,18 @@
+import samplePerson from '../data/sample.json'
+
 
 function Profile() {
+  const profile = samplePerson.profile;
   return (
       <div className="profile">
-        Profile
+        <div className="profileRow1">
+          <div className="profileCol1"> 
+          <h1>{profile.name}</h1>
+          </div>
+
+          <div className="profileCol2"> 
+          </div>
+        </div>
       </div>
   );
 }
@@ -16,12 +26,30 @@ function Skills() {
   );
 }
 
+function WorkExperience(){
+  return(
+    <div className="workExperience">
+    Work Experience
+  </div>
+  );
+}
+
+function Education(){
+  return(
+    <div className="education">
+    Education
+  </div>
+  );
+}
+
 
 export default function CvPage() {
     return (
       <div className="cvPage">
         <Profile />
         <Skills />
+        <WorkExperience />
+        <Education />
       </div>
     );
 }
